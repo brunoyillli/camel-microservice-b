2ffa5b9ed74f503f8a75790f1d2b5bbe7a578865
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-07-05 15:38:42 -0300

    criando projeto

---
+++ src/main/java/io/github/brunoyillli/camelmicroserviceb/CamelMicroserviceBApplication.java
@@ -0,0 +1,13 @@
+package io.github.brunoyillli.camelmicroserviceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMicroserviceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMicroserviceBApplication.class, args);
+	}
+
+}
