0dd11c0ed25cc480ea19b060494b4da6c2f88b28
brunoyillli <wolfteam1910@gmail.com>
2023-07-07 11:37:47 -0300

    pegando os arquivos do kafka pelo camel

---
+++ src/main/java/io/github/brunoyillli/camelmicroserviceb/routes/KafkaMqReceiverRouter.java
@@ -0,0 +1,21 @@
+package io.github.brunoyillli.camelmicroserviceb.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.stereotype.Component;
+
+import io.github.brunoyillli.camelmicroserviceb.CurrencyExchange;
+
+@Component
+public class KafkaMqReceiverRouter extends RouteBuilder{
+	
+	@Override
+	public void configure() throws Exception {
+
+		from("kafka:myKafkaTopic")
+		.unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+		.to("log:received-message-from-kafka");
+		
+	}
+
+}
